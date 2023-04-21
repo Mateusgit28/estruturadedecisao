c93d13e8644ad2592ad9bb193c4e534dfe16c061
Mateusgit28 <110639736+Mateusgit28@users.noreply.github.com>
2023-04-20 21:32:34 -0300

    projetinho

--- projetoguto/index.php
+++ projetoguto/index.php
@@ -1,41 +1,73 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Vendas de Jogos</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-</head>
-
 <body>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Venda de Jogos</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        
-      </ul>
-      <form action="login.php" method="post" class="d-flex">
-        <input class="form-control me-2" type="email" placeholder="email" >
-        <button class="btn btn-outline-success" type="submit">Entrar</button>
-      </form>
+  <?php
+  include "menu.php";
+  include "cabecalho.php";
+  ?>
+
+  <div class="container-fluid banner">
+    <div id="carouselExampleIndicators" class="carousel slide carousel-fade">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="img/forza.png" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="img/rdr2.png" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="img/seaof.png" class="d-block w-100" alt="...">
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
     </div>
   </div>
-</nav>
+  <div class="container">
+    <div class="row">
+      <div class="col-12 text-center"><h1>Jogos em Destaque</h1></div>
+      <div class="col-12 text-center"><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi, debitis optio. Sit reiciendis consequuntur et, culpa dignissimos voluptatem ex. Earum labore sed reiciendis. Quis dolores fuga, recusandae cum accusantium ea!</p></div>
+      <div class="row">
+        <div class="col-3 text-center">
+          <img src="img/paydayy.png" class="img-fluid">
+          <h2>Nome do jogo</h2>
+          <a href="nomedojogo.php" class="btn btn-primary">VER MAIS</a>
+        </div>
+        <div class="col-3 text-center">
+          <img src="img/pung.jpg" class="img-fluid">
+          <h2>Nome do jogo</h2>
+          <a href="nomedojogo.php" class="btn btn-primary">VER MAIS</a>
+        </div>
+        <div class="col-3 text-center">
+          <img src="img/farcry.jpg" class="img-fluid">
+          <h2>Nome do jogo</h2>
+          <a href="nomedojogo.php" class="btn btn-primary">VER MAIS</a>
+        </div>
+        <div class="col-3 text-center">
+          <img src="img/warzone.jpg" class="img-fluid">
+          <h2>Nome do jogo</h2>
+          <a href="nomedojogo.php" class="btn btn-primary">VER MAIS</a>
+        </div>
+      </div>
+    </div>
+
+  
+  </div>
 
 
 
 
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 
 </html>
