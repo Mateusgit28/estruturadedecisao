46e3e173b718ba035be2b44698a315752b7b4169
Mateusgit28 <110639736+Mateusgit28@users.noreply.github.com>
2023-05-04 21:56:24 -0300

    lista de usuarios

---
+++ projetoguto/listar-usuarios.php
@@ -0,0 +1,44 @@
+<?php
+include "cabecalho.php";
+include "menu-sistema.php";
+
+?>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>Lista de usuario</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <table border="1">
+                <tr>
+                    <td>Id</td>
+                    <td>Nome</td>
+                    <td>Email</td>
+                    <td>Ações</td>
+                    
+                </tr>
+                <?php
+                include "conexao.php";
+                $sql_buscar = "select * from usuario";
+                $todos_os_usuario = mysqli_query($conexao, $sql_buscar);
+                while ($um_usuario = mysqli_fetch_assoc($todos_os_usuario)) :
+                ?>
+                    <tr>
+                        <td> <?php echo $um_usuario["id"]; ?> </td>
+                        <td> <?php echo $um_usuario["nome"]; ?> </td>
+                        <td> <?php echo $um_usuario["email"]; ?> </td>
+                        
+                    </tr>
+                <?php
+
+
+
+
+
+
+                endwhile;
+                mysqli_close($conexao);
+                ?>
+            </table>
